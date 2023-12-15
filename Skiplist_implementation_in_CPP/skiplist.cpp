da33283e4d1c8f94c9f0a0d5f58cd93e7287b0e6
PrathamGupta423 <Prathamgupta423@gmail.com>
2023-12-15 05:32:18 +0530

    added search

--- Skiplist_implementation_in_CPP/skiplist.cpp
+++ Skiplist_implementation_in_CPP/skiplist.cpp
@@ -2,6 +2,8 @@
 #include<vector>
 #include <limits>
 #include <cstdlib> 
+#include <ctime>
+
 
 class Node
 {
@@ -17,7 +19,7 @@ public:
         this->down = down;
     }
     int getData(){
-        return this->data;
+        return this->data;  
     }
     Node *getNext(){
         return this->next;
@@ -61,11 +63,32 @@ public:
         return;
     }
     ~Node(){
-        std::cout << "Node with data " << this->data << " is deleted" << std::endl;
+        // std::cout << "Node with data " << this->data << " is deleted" << std::endl;
+    }
+    int getindex(int n){
+        Node *temp = this;
+        int index = 0;
+        while (index = n or temp->next != NULL)
+        {
+            temp = temp->next;
+            index++;
+        }
+        if (index == n){
+            return this->data;
+        }
+        else{
+            return -1;
+        }
+    }
+    Node* bottomMost(){
+        Node *temp = this;
+        while(temp->down != NULL){
+            temp = temp->down;
+        }
+        return temp;
     }
 
 };
-
 class skiplist
 {
 private:
@@ -79,7 +102,7 @@ public:
         Node *newNode = new Node(std::numeric_limits<int>::min(), head, NULL);
         levels.push_back(newNode);
         for (int i = 1; i < maxLevel; i++){
-            Node *newNode = new Node(std::numeric_limits<int>::min(), NULL, NULL);
+            Node *newNode = new Node(std::numeric_limits<int>::min(), NULL, levels[i-1]);
             levels.push_back(newNode);
         }
         for (int i = 0; i < maxLevel-1; i++)
@@ -93,6 +116,9 @@ public:
     void skipify(Node * bottom , Node * top){
         Node *temp = top;
         Node *bottomTemp = bottom;
+        if (bottomTemp->getNext() == NULL){
+            
+        }
         bottomTemp = bottomTemp->getNext();
         for (; bottomTemp != NULL; bottomTemp = bottomTemp->getNext()){
             if (rand() % 2 == 0){
@@ -100,11 +126,7 @@ public:
                 temp->setnext(newNode);
                 temp = temp->getNext();
             }
-            
-            
         }
-
-        
     }
 
     ~skiplist(){
@@ -112,7 +134,6 @@ public:
             (levels[i]) -> freeSLL();
         }
         std::cout << "Skiplist is deleted" << std::endl;
-
     }
 
     void print(){
@@ -125,35 +146,81 @@ public:
             std::cout << std::endl;
         }
     }
+    Node* search(int key){
+        Node *temp = levels[maxLevel-1];
+        // std::cout<<"searching"<<temp->getData()<<std::endl;
+        // return temp -> getNext();
+        while(temp->getNext()!= NULL or temp->getDown() != NULL){
+            // std::cout<<"in loop"<<temp->getData()<<std::endl;
+            if (temp->getData() == key){
+                return temp ->bottomMost();
+                // std::cout<<"1st if"<<temp->getData()<<std::endl;
+            }
+            else if (temp->getData() < key){
+                // std::cout<<"in else"<<temp->getData()<<std::endl;
+                if (temp->getNext() != NULL){
+                    if (temp -> getNext() -> getData() <= key)
+                    {
+                        temp = temp -> getNext();
+                    }
+                    else if (temp->getDown() != NULL){
+                        temp = temp -> getDown();
+                    }
+                    else{
+                        break;
+                    }
+                }
+                else if (temp->getDown() != NULL){
+                    temp = temp -> getDown();
+                }
+                else{
+                    break;
+                }
+            }    
+            else{
+                break;
+            }
+        }
+        return NULL;
+    }
+    
 
 
 };
-
 int main(){
-
-    int len = 25;
-
+    srand(time(NULL));
+    int len = 1000;
     int *num = new int[len];
-
     for (int i = 0; i < len; i++)
     {
         num[i] = i;
     }
-
-    Node *head = new Node(1, NULL, NULL);
+    Node *head = new Node(0, NULL, NULL);
     Node *newNode;
-
     for (int i = 1; i < len; i++)
     {
         newNode = new Node(num[i], NULL, NULL);
         (*head).append(newNode);
     }
-    // head -> freeSLL();
+
+    free(num);
 
     
-    skiplist *sl = new skiplist(4 , head);
+    skiplist *sl = new skiplist(20, head);
     sl -> print();
-    (*sl).~skiplist();
+    Node* b = sl -> search(101);
+    if (b == NULL){
+        std::cout<<"not found"<<std::endl;
+    }
+    else{
+        std::cout<<"found"<<std::endl;
+    }
+
+    // std::cout<<"searched"<<" "<<b -> getData()<<std::endl;
+    // delete b;
+
+    delete sl;
+
 
     return 0;
 }
