d968b42f1aa9a803d0a2f8e6dc5c9aaa47851dce
PrathamGupta423 <Prathamgupta423@gmail.com>
2023-12-18 04:20:33 +0530

    added fully working cost function

--- Graph_implementation_in_CPP/Graph.cpp
+++ Graph_implementation_in_CPP/Graph.cpp
@@ -58,12 +58,20 @@ void printPath(std::vector<std::pair<int , int>>path){
         std::cout << path[i].first << " " << path[i].second << std::endl;
     }
 }
+void printUsedEges(std::vector<std::pair< std::pair<int, int> , std::pair<int, int>>> Used_edges){
+    for(int i=0; i<Used_edges.size(); i++){
+        std::cout << Used_edges[i].first.first << " " << Used_edges[i].first.second << " " << Used_edges[i].second.first << " " << Used_edges[i].second.second << std::endl;
+    }
+}
 // There are multiple ways of implementing a lower bound 
 // We will first try to implement for a symmetric TSP --> adj[i][j] = adj[j][i]
 float lowerBoundCost(float **adj, int V, std::vector<std::pair<int , int>>path= {}){
     float cost = 0;
+    // left for visualization
     std::vector<std::vector<int>> What_was_removed(V, std::vector<int>(V, 0));
     
+    std::vector<std::pair< std::pair<int, int> , std::pair<int, int>>> Used_edges;
+
     for (int i = 0; i < V; i++)
     {
         float min = std::numeric_limits<float>::infinity();
@@ -87,15 +95,47 @@ float lowerBoundCost(float **adj, int V, std::vector<std::pair<int , int>>path= 
         }
         cost += 0.5*(min+min2);
         What_was_removed[i][min_index] = 1;
+        Used_edges.push_back(std::make_pair(std::make_pair(i, min_index),std::make_pair(i, min2_index)));
         What_was_removed[i][min2_index] = 2;
-
+        // Used_edges.push_back(std::make_pair(i, min2_index));
+        
         // std::cout << "min: " << min << " min2: " << min2 << std::endl;
         // std::cout << "min_index: " << min_index << " min2_index: " << min2_index << std::endl;
         // std::cout << "cost: " << cost << std::endl;            
     }
 
-    printMatrix(What_was_removed, V);
+    // printMatrix(What_was_removed, V);
+    printUsedEges(Used_edges);
+    std::cout << "cost: " << cost << std::endl;
 
+    for (int i = 0; i < path.size(); i++)
+    {
+        int node = path[i].first;
+        int next_node = path[i].second;
+
+        if(Used_edges[node].first.first == node && Used_edges[node].first.second == next_node){
+            continue;
+        }
+        else if(Used_edges[node].second.first == node && Used_edges[node].second.second == next_node){
+            continue;
+        }
+        else{
+            cost += 0.5*adj[node][next_node];
+            cost -= 0.5*adj[Used_edges[node].second.first][Used_edges[node].second.second];
+            std::cout << "cost: " << cost << std::endl;
+            Used_edges[node].second.first = Used_edges[node].first.first;
+            Used_edges[node].second.second = Used_edges[node].first.second;
+            // std::cout << "change has occured: " << std::endl;
+            // printUsedEges(Used_edges);
+            Used_edges[node].first.first = node;
+            Used_edges[node].first.second = next_node;
+            std::cout << "change has occured: " << std::endl;
+            printUsedEges(Used_edges);
+
+
+        }
+    }
+    std::cout << "cost: " << cost << std::endl;
     
     return cost;
 
@@ -118,7 +158,7 @@ int main(){
     g -> addEdge(3,4,6);
     g -> printGraph();  
     std::cout <<lowerBoundCost(g->adj, g->V)<<std::endl;
-    std::vector< std::pair <int,int>> path = {{0,1},{1,2},{2,3},{3,4},{4,0}};
+    std::vector< std::pair <int,int>> path = {{0,2},{0,4}};
     std::cout <<lowerBoundCost(g->adj, g->V, path)<<std::endl;
     std::cout << "Path: " << std::endl;
     printPath(path);
